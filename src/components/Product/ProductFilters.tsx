af78999abc5e4df3c073276bcbf617d6b6d4fa56
Jean Anselme <jean66.anselme@gmail.com>
2024-07-27 14:56:57 +0000

    fix responsive bug

--- src/components/Product/ProductFilters.tsx
+++ src/components/Product/ProductFilters.tsx
@@ -49,52 +49,55 @@ const ProductFilters = () => {
     };
 
     return (
-        <div className="flex flex-col md:flex-row md:space-x-4 mb-4">
-            <input
-                type="text"
-                placeholder="Search Products"
-                value={searchTerm}
-                onChange={handleSearchChange}
-                className="border rounded p-2 mb-2 md:mb-0"
-            />
-            <select onChange={handleCategoryChange} className="border rounded p-2 mb-2 md:mb-0">
-                <option value="">All Categories</option>
-                <option value="electronics">Electronics</option>
-                <option value="jewelery">Jewelry</option>
-                <option value="men's clothing">Men's Clothing</option>
-                <option value="women's clothing">Women's Clothing</option>
-            </select>
-            <div className="flex space-x-2 mb-2 md:mb-0">
+        <div className="container">
+            <div className="flex flex-col md:flex-row md:space-x-4 mb-4">
                 <input
-                    type="number"
-                    name="min"
-                    value={priceRange[0]}
-                    onChange={handlePriceChange}
-                    placeholder="Min Price"
-                    className="border rounded p-2"
+                    type="text"
+                    placeholder="Search Products"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    className="border rounded p-2 mb-2 md:mb-0"
                 />
-                <input
-                    type="number"
-                    name="max"
-                    value={priceRange[1]}
-                    onChange={handlePriceChange}
-                    placeholder="Max Price"
-                    className="border rounded p-2"
-                />
+                <select onChange={handleCategoryChange} className="border rounded p-2 mb-2 md:mb-0">
+                    <option value="">All Categories</option>
+                    <option value="electronics">Electronics</option>
+                    <option value="jewelery">Jewelry</option>
+                    <option value="men's clothing">Men's Clothing</option>
+                    <option value="women's clothing">Women's Clothing</option>
+                </select>
+                <div className="flex flex-col gap-2 md:flex-row md:space-x-2 mb-2 md:mb-0 ">
+                    <input
+
+                        type="number"
+                        name="min"
+                        value={priceRange[0]}
+                        onChange={handlePriceChange}
+                        placeholder="Min Price"
+                        className="border rounded p-2"
+                    />
+                    <input
+                        type="number"
+                        name="max"
+                        value={priceRange[1]}
+                        onChange={handlePriceChange}
+                        placeholder="Max Price"
+                        className="border  rounded p-2"
+                    />
+                </div>
+                <select onChange={handleRatingChange} className="border rounded p-2 mb-2 md:mb-0">
+                    <option value="0">All Ratings</option>
+                    <option value="1">1 Star & Up</option>
+                    <option value="2">2 Stars & Up</option>
+                    <option value="3">3 Stars & Up</option>
+                    <option value="4">4 Stars & Up</option>
+                </select>
+                <select onChange={handleSortChange} className="border rounded p-2 mb-2 md:mb-0">
+                    <option value="default">Default</option>
+                    <option value="priceAsc">Price: Low to High</option>
+                    <option value="priceDesc">Price: High to Low</option>
+                    <option value="popularity">Popularity</option>
+                </select>
             </div>
-            <select onChange={handleRatingChange} className="border rounded p-2 mb-2 md:mb-0">
-                <option value="0">All Ratings</option>
-                <option value="1">1 Star & Up</option>
-                <option value="2">2 Stars & Up</option>
-                <option value="3">3 Stars & Up</option>
-                <option value="4">4 Stars & Up</option>
-            </select>
-            <select onChange={handleSortChange} className="border rounded p-2 mb-2 md:mb-0">
-                <option value="default">Default</option>
-                <option value="priceAsc">Price: Low to High</option>
-                <option value="priceDesc">Price: High to Low</option>
-                <option value="popularity">Popularity</option>
-            </select>
         </div>
     );
 };
